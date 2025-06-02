1939833f045d49166c0f695f1d007e56ebd3ff95
mruszkiewicz-dev <mruszkiewicz.dev@gmail.com>
2025-06-02 14:08:02 +0200

    up 2 calBMI

--- 2/2_calcBMI.py
+++ 2/2_calcBMI.py
@@ -2,7 +2,17 @@ weight = input("Podaj wagę")
 growth = input("Podaj wzrost") # trzeba zamienić na metry
 
 
-bmi = float(weight) / float(growth)
+bmi = float(weight) / ((float(growth) / 100) ** 2)
 
+
+
+if bmi < 18.5:
+    print("Niedowaga")
+elif bmi < 25:
+    print("waga prawidłowa")
+elif bmi < 30:
+    print("nadwaga")
+else:
+    print("otyłość")
 
 print(bmi)
